83b31cb4ce0702ec0f4c1616da18725a423d695b
shandamelcher <git@shananda.com>
2015-10-07 20:30:57 -0700

    added some math

--- TankDrive/TankDrive.ino
+++ TankDrive/TankDrive.ino
@@ -1,16 +1,25 @@
 int ch1;
 int ch2;
+float output1;
+float output2;
+
+
 #define inputMax 2575
 #define inputMin 1285
 #define inputScaler ((inputMax - inputMin)/2)
-#define inputMedian (scaler + inputMin)
+#define inputMedian (inputScaler + inputMin)
+
+//channel 3, 
+#define LeftYPin 43
+//channel 1
+#define RightXPin 53
 
 
 void setup() {
   // put your setup code here, to run once:
 
-pinMode(53, INPUT); // Set our input pins as such
-pinMode(54, INPUT); // Set our input pins as such
+pinMode(LeftYPin, INPUT); // Set our input pins as such
+pinMode(RightXPin, INPUT); // Set our input pins as such
 
 Serial.begin(9600); // The author made a ridiculous pun. We don't know what this does.
 
@@ -19,11 +28,17 @@ Serial.begin(9600); // The author made a ridiculous pun. We don't know what this
 void loop() {
   // put your main code here, to run repeatedly:
 
-  ch1 = pulseIn(53, HIGH, 25000); // Read the pulse width of each channel
-  ch2 = pulseIn(54, HIGH, 25000); // Read the pulse width of each channel
-  
-  //Serial.print("Current value: ");  // Print the value of each channel
-  //Serial.println(ch1);
+  ch1 = pulseIn(LeftYPin, HIGH, 25000); // Read the pulse width of each channel
+  ch2 = pulseIn(RightXPin, HIGH, 25000); // Read the pulse width of each channel
 
-  delay(400); // Make the terminal window happier
+  output1 = (ch1 - inputMedian); //inputScaler;
+  output2 = (ch2 - inputMedian); //inputScaler;
+
+  analogWrite(55, output1);
+  analogWrite(56, output2);
+  
+  Serial.print("Output value: ");  // Print the value of each channel
+  Serial.println(ch1);
+  Serial.println(ch2);
+
 }
