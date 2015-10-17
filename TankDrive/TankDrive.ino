29933efa850487fee2f3c5ff2a05a748e471b6ef
shandamelcher <git@shananda.com>
2015-10-16 17:17:46 -0700

    added output conversion for scale of 0 to 255

--- TankDrive/TankDrive.ino
+++ TankDrive/TankDrive.ino
@@ -10,9 +10,9 @@ float inputMedian;
 #define RangeMax 10000
 #define RangeMin 0
 //channel 3, 
-#define LeftYPin 43
+#define LeftYPin 35
 //channel 1
-#define RightXPin 53
+#define RightXPin 45
 
 
 void setup() {
@@ -28,8 +28,11 @@ inputMin = RangeMax;
 Serial.begin(9600); 
 }
 
+
+
 void loop() {
   while(true){
+
   //set motor speeds
   analogWrite(55, output1);
   analogWrite(56, output2);
@@ -37,8 +40,12 @@ void loop() {
   // Read the pulse width of each channel
   ch1 = pulseIn(LeftYPin, HIGH, 25000); 
   ch2 = pulseIn(RightXPin, HIGH, 25000); 
-
-  if ((ch1 = 0) || (ch2 = 0))
+  //Serial.print("ch 1: ");  
+  //Serial.println(ch1);
+  //Serial.print("ch 2: ");
+  //Serial.println(ch2);
+  
+  if ((ch1 == 0) || (ch2 == 0))
     {//stop now 
     output1 = 0;
     output2 = 0;
@@ -56,16 +63,33 @@ void loop() {
         inputMin = ch1;
       }
 
+        //Serial.print("inputmax 1: ");  
+        //Serial.println(inputMax);
+        //Serial.print("inputmin 2: ");
+        //Serial.println(inputMin);
+        
       inputScaler = ((inputMax - inputMin)/2);
       inputMedian = (inputScaler + inputMin);
+      //Serial.print("inputscaler 1: ");  
+      //Serial.println(inputScaler);
+      //Serial.print("inputmedian 2: ");
+      //Serial.println(inputMedian);
     }
 
+  //convert to range of -1 to 1
   output1 = (ch1 - inputMedian)/inputScaler;
   output2 = (ch2 - inputMedian)/inputScaler;
+
+  //convert to output range of 0 to 255
+  output1 = ((output1 * 128) + 128);
+  output2 = ((output2 * 128) + 128);
   
-  Serial.print("Output value: ");  // Print the value of each channel
-  Serial.println(ch1);
-  Serial.println(ch2);
+  Serial.print("Output 1: ");  
+  Serial.println(output1);
+  Serial.print("Output 2: ");
+  Serial.println(output2);
+  
+  delay(1000);
   }
 }
 
