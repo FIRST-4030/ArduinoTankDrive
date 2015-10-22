9d37a20b510370091bcc1965b6055f89df0d76be
Ariel <raarielgrace@gmail.com>
2015-10-21 20:46:31 -0700

    more clean-up.

--- TankDrive/TankDrive.ino
+++ TankDrive/TankDrive.ino
@@ -1,122 +1,133 @@
-#define STOP 0
-#define SERIAL_SPEED 9600
-#define rangeMax 5000
-#define rangeMin 500
-// Receiver hannel 3
-#define LeftYPin 35
-// Receiver channel 1
-#define RightXPin 45
-#define OutputScaler (255/2)
-#define DeadZone 0.05
+// Comment this debug line for production runs
+#define DEBUG
 
-int ch1;
-int ch2;
-float output1 = 0;
-float output2 = 0;
-float inputMax;
-float inputMin;
+#define STOP (0)
+#define SERIAL_SPEED (9600)
+#define IN_RANGE_MAX (5000)
+#define IN_RANGE_MIN (500)
+#define PIN_Y (35)
+#define PIN_X (45)
+#define OUTPUT_SCALER (255/2)
+#define DEAD_ZONE (5)
+#define OUTPUT_LEFT (55)
+#define OUTPUT_RIGHT (56)
+#define READ_TIMEOUT (25000)
+
+// Globals
+int inY;
+int inX;
+float outLeft = STOP;
+float outRight = STOP;
+float inputMax = IN_RANGE_MIN;
+float inputMin = IN_RANGE_MAX;
 float inputScaler;
 float inputMedian;
 
-void setup();
-void loop();
-void CheckMinMax(float input);
+// Function prototypes
+void setMinMax(float input);
+int deadZone(int in);
+float limitOutput(float in);
 
 void setup() {
   // Set our input pins as such
-  pinMode(LeftYPin, INPUT); 
-  pinMode(RightXPin, INPUT); 
+  pinMode(PIN_Y, INPUT); 
+  pinMode(PIN_X, INPUT); 
 
-  inputMax = rangeMin;
-  inputMin = rangeMax;
-  output1 = STOP;
-  output2 = STOP;
+  // Ensure our initial state is reasonable
+  inputMax = IN_RANGE_MIN;
+  inputMin = IN_RANGE_MAX;
+  outLeft = STOP;
+  outRight = STOP;
 
+  // Pull up the serial port for debugging
   Serial.begin(SERIAL_SPEED); 
 }
 
 void loop() {
-  while(true){
+while(true) {
 
-  //set motor speeds
+  // Set motor speeds
   Serial.print("Output: ");  
-  Serial.print(output1);
+  Serial.print(outLeft);
   Serial.print(",");
-  Serial.print(output2);
-  analogWrite(55, output1);
-  analogWrite(56, output2);
+  Serial.println(outRight);
+  analogWrite(OUTPUT_RIGHT, outLeft);
+  analogWrite(OUTPUT_LEFT, outRight);
 
   // Read the pulse width of each channel
-  ch1 = pulseIn(LeftYPin, HIGH, 25000); 
-  ch2 = pulseIn(RightXPin, HIGH, 25000); 
+  inY = pulseIn(PIN_Y, HIGH, READ_TIMEOUT); 
+  inX = pulseIn(PIN_X, HIGH, READ_TIMEOUT);
+  Serial.print("Raw: ");
+  Serial.print(inY);
+  Serial.print(",");
+  Serial.println(inX);
 
-  if ((ch1 <= rangeMin) || (ch2 <= rangeMin) || (ch1 >= rangeMax) || (ch2 >= rangeMax)){
-    //stop now 
-    output1 = 0;
-    output2 = 0;
-    continue; 
-
-    //check min max
-    CheckMinMax(ch1);
-    CheckMinMax(ch2);
-        
-    inputScaler = ((inputMax - inputMin)/2);
-    inputMedian = (inputScaler + inputMin);
-    }
-
-    
-  //convert to range of -1 to 1
-  output1 = (ch1 - inputMedian)/inputScaler;
-  output2 = (ch2 - inputMedian)/inputScaler;
-  
-  //limit output values to range
-  if (abs(output1) > 1){
-     if (output1 < 0){
-      output1 = -1;
-     }
-     else{ 
-      output1 = 1;
-     }
-   }
-  if (abs(output2) > 1){
-     if (output2 < 0){
-      output2 = -1;
-     }
-     else {
-      output2 = 1;
-     }
-   }
-  
-    //make the center 0 point slightly larger
-  if ((output1 < DeadZone) && (output1 > -DeadZone))
-    {
-    output1 = 0;
-    }
-  if ((output2 < DeadZone) && (output2 > -DeadZone))
-    { 
-    output2 = 0;
-    }
-    
-  //convert to output range of 0 to 255
-  output1 = ((output1 * OutputScaler) + OutputScaler);
-  output2 = ((output2 * OutputScaler) + OutputScaler);
-
-  Serial.print("Output 1: ");  
-  Serial.println(output1);
-  Serial.print("Output 2: ");
-  Serial.println(output2);
-  
-  delay(1000);
+  // On invalid input, stay in a tight loop
+  if ((inY <= IN_RANGE_MIN) || (inX <= IN_RANGE_MIN) ||
+    (inY >= IN_RANGE_MAX) || (inX >= IN_RANGE_MAX)) {
+      outLeft = STOP;
+      outRight = STOP;
+      #ifdef DEBUG
+        Serial.println("Invalid input");
+        delay(1000);
+      #endif
+      continue;
   }
+
+  // Calculate the input range
+  setMinMax(inY);
+  setMinMax(inX);
+
+  // Zero-center the input
+  inY -= inputMedian;
+  inX -= inputMedian;
+
+  // Provide a small dead zone on the input data
+  inY = deadZone(inY);
+  inX = deadZone(inX);
+  
+  // Convert to range of -1 to 1
+  outLeft = inY / inputScaler;
+  outRight = inX / inputScaler;
+  
+  // Limit output values to the valid range, in case our scaler is slightly off
+  outLeft = limitOutput(outLeft);
+  outRight = limitOutput(outRight);
+      
+  // Scale to the output range
+  outLeft = ((outLeft * OUTPUT_SCALER) + OUTPUT_SCALER);
+  outRight = ((outRight * OUTPUT_SCALER) + OUTPUT_SCALER);
+
+  #ifdef DEBUG
+    delay(1000);
+  #endif
+}
 }
 
-void CheckMinMax(float input){
-     if (input > inputMax) 
-      {
-        inputMax = input;
-      }
-     else if (input < inputMin) 
-      {
-        inputMin = input;
-      }
+void setMinMax(float input){
+  if (input > inputMax) {
+    inputMax = input;
+  } else if (input < inputMin) {
+    inputMin = input;
+  }
+  inputScaler = ((inputMax - inputMin) / 2);
+  inputMedian = (inputScaler + inputMin);
+}
+
+int deadZone(int in) {
+  if (abs(in) < DEAD_ZONE) {
+    in = 0;
+  }
+  return in;
+}
+
+float limitOutput(float in) {
+  float out = in;
+  if (abs(in) > 1){
+    out = 1;
+    if (in < 0) {
+      out *= -1;
+    }
+  }
+  return out;
 }
